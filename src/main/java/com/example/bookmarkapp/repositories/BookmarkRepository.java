591cab3d50c9b9387d429c2e01bb481d65127e83
tiho2 <tihomir2e@gmail.com>
2021-03-03 22:42:36 +0100

    refactoring: - unused SpringFoxConfig.java removed - at the moment openApi is used. - unused imports removed.

--- src/main/java/com/example/bookmarkapp/repositories/BookmarkRepository.java
+++ src/main/java/com/example/bookmarkapp/repositories/BookmarkRepository.java
@@ -3,7 +3,6 @@ package com.example.bookmarkapp.repositories;
 import com.example.bookmarkapp.model.Bookmark;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
 
 @Service
 public interface BookmarkRepository extends ReactiveMongoRepository<Bookmark, String> {
