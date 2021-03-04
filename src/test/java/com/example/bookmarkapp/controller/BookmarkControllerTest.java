881a7e4c44a553744ca6ff7eaca155f96ebff807
tiho2 <tihomir2e@gmail.com>
2021-03-04 22:39:59 +0100

    BookmarkControllerTest - test case implemented. - minor refactoring

--- src/test/java/com/example/bookmarkapp/controller/BookmarkControllerTest.java
+++ src/test/java/com/example/bookmarkapp/controller/BookmarkControllerTest.java
@@ -1,24 +1,29 @@
 package com.example.bookmarkapp.controller;
 
+import com.example.bookmarkapp.model.Bookmark;
 import com.example.bookmarkapp.repositories.BookmarkRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
 class BookmarkControllerTest {
 
     WebTestClient webTestClient;
     BookmarkRepository bookmarkRepository;
     BookmarkController bookmarkController;
+
     @BeforeEach
     void setUp() {
         bookmarkRepository = Mockito.mock(BookmarkRepository.class);
         bookmarkController = new BookmarkController(bookmarkRepository);
-        webTestClient = WebTestClient.bindToController(bookmarkRepository).build();
+        webTestClient = WebTestClient.bindToController(bookmarkController).build();
     }
 
     @AfterEach
@@ -26,19 +31,15 @@ class BookmarkControllerTest {
     }
 
     @Test
-    void listMyBookmarks() {
-    }
+    void listBookmarks() {
+        BDDMockito.given(bookmarkRepository.findAll())
+                .willReturn(Flux.just(Bookmark.builder().url("http://spring.io").owner("user").shared(Boolean.TRUE).build(),
+                        Bookmark.builder().url("http://docs.spring.io").owner("user").shared(Boolean.FALSE).build()));
 
-    @Test
-    void listMySharedBookmarks() {
-    }
-
-    @Test
-    void listOtherBookmarks() {
-    }
-
-    @Test
-    void listSharedBookmarks() {
+        webTestClient.get().uri("http://localhost:8080/bookmarks?scope=&scopeDetails=")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectBodyList(Bookmark.class);
     }
 
     @Test
@@ -51,5 +52,18 @@ class BookmarkControllerTest {
 
     @Test
     void update() {
+    }
+
+
+    @Test
+    void testGetById() {
+    }
+
+    @Test
+    void patchBookmark() {
+    }
+
+    @Test
+    void testUpdate() {
     }
 }
